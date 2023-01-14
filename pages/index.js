665ff4307a862640667030e49fcca40b61f72a7e
2-fly-4-ai <brian@serp.ai>
2023-01-15 00:25:58 +1300

    gtm fix

--- pages/index.js
+++ pages/index.js
@@ -236,7 +236,7 @@ export default function Home({ data }) {
                         />
 
                         <div className="py-2 cursor-pointer mt-auto my-1 uppercase px-7 justify-center text-sm font-medium text-gray-700 bg-white rounded-full border-2 border-gray-200 hover:bg-gray-100 hover:text-blue-700  focus:outline-none  dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
-                          <a className="select-none " href={tag.uri}>
+                          <a className="" href={tag.uri}>
                             Read more
                           </a>
                           <svg
