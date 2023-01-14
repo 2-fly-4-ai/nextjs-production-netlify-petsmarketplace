8034a5e85d5846e86b51cf7bec13682e2f9d5063
2-fly-4-ai <brian@serp.ai>
2023-01-14 18:40:45 +1300

    added prefetch = false

--- src/components/product_category/subcategory/index.js
+++ src/components/product_category/subcategory/index.js
@@ -20,7 +20,7 @@ export default function subcategory({ data }) {
                 >
                   <a
                     href={child.uri}
-                    className="text-gray-600 dark:text-gray-400 border-2 dark:border-gray-700 focus:ring-2 focus:outline-none focus:ring-primary-300 focus:rounded-full border-gray-300 rounded-full px-2 text-sm py-0.5 pb-1.5 bg-white dark:bg-gray-800 hover:bg-gray-200 capitalize font-medium"
+                    className="whitespace-nowrap text-gray-600 dark:text-gray-400 border-2 dark:border-gray-700 focus:ring-2 focus:outline-none focus:ring-primary-300 focus:rounded-full border-gray-300 rounded-full px-2 text-sm py-0.5 pb-1.5 bg-white dark:bg-gray-800 hover:bg-gray-200 capitalize font-medium"
                     dangerouslySetInnerHTML={{
                       __html: sanitize(child.name ?? {}),
                     }}
