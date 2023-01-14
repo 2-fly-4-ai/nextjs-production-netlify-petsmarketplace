95a7c713374e6a65b4ebee15c066993bae2bb522
2-fly-4-ai <brian@serp.ai>
2023-01-14 18:20:45 +1300

    added prefetch = false

--- pages/index.js
+++ pages/index.js
@@ -219,10 +219,10 @@ export default function Home({ data }) {
             </div>
             <div className="grid gap-6 mb-16  lg:grid-cols-5">
               {!isEmpty(data?.productTags?.nodes)
-                ? data?.productTags?.nodes.map((tag) => {
+                ? data?.productTags?.nodes.map((tag, index) => {
                     return (
                       <article
-                        key={tag.name}
+                        key={index}
                         className="border-1 p-3  justify-center content-center flex-col flex"
                       >
                         <h2 className="my-1 text-2xl  text-center tracking-tight text-gray-900 dark:text-white capitalize">
@@ -267,9 +267,12 @@ export default function Home({ data }) {
             </div>
             <div className="grid gap-6 lg:gap-0  md:grid-cols-3 lg:grid-cols-6 sm:grid-cols-2 justify-center">
               {!isEmpty(data?.productTaxonomies?.nodes)
-                ? data?.productTaxonomies?.nodes.map((tag) => {
+                ? data?.productTaxonomies?.nodes.map((tag, index) => {
                     return (
-                      <div className="p-3 pb-3 max-w-sm bg-white rounded-none  dark:bg-gray-800 dark:border-gray-700 cursor-pointer">
+                      <div
+                        className="p-3 pb-3 max-w-sm bg-white rounded-none  dark:bg-gray-800 dark:border-gray-700 cursor-pointer"
+                        key={index}
+                      >
                         {!isEmpty(
                           tag.products?.nodes[0]?.single_product_acf
                             ?.productImageMainUrl
