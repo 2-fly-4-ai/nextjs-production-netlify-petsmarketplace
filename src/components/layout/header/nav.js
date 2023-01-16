6d658b127c87aa86fea25e1400070169c24748d8
2-fly-4-ai <brian@serp.ai>
2023-01-17 08:27:08 +1200

    meta fix

--- src/components/layout/header/nav.js
+++ src/components/layout/header/nav.js
@@ -7,7 +7,6 @@ import Image from "next/image";
 import NavSearch from "../../search/nav-search";
 
 const Nav = ({ footer, header, headerMenus, slug }) => {
-  console.warn(footer);
   const [isMenuVisible, setMenuVisibility] = useState("");
   if (isEmpty(headerMenus)) {
     return null;
