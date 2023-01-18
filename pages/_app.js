7eda5563a28a439aa4e06098cb98785ae0d9dbf0
2-fly-4-ai <brian@serp.ai>
2023-01-19 11:17:36 +1200

    netlify new gtm approach

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import "../src/styles/index.scss";
 import { ApolloProvider } from "@apollo/client";
 import client from "../src/apollo/client";
 import Script from "next/script";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 // import Router from 'next/router';
 // import NProgress from 'nprogress';
@@ -13,27 +15,13 @@ import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   const GTM_ID = "GTM-WQLRZPZ";
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-WQLRZPZ" });
+  }, []);
 
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
-      <Script id="gtm-script">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-      `}
-      </Script>
-      <noscript>
-        <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-          height="0"
-          width="0"
-          className="display:none"
-        ></iframe>
-      </noscript>
     </ApolloProvider>
   );
 }
