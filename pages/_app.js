44b37a999a52acffbcdbd8a63fcf77c56f3856c3
2-fly-4-ai <brian@serp.ai>
2023-01-20 21:04:47 +1200

    added nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,12 @@ import { useEffect } from "react";
 import TagManager from "react-gtm-module";
 
 import Router from "next/router";
-import NProgress from "nprogress";
+// import NProgress from "nprogress";
 
-NProgress.configure({ showSpinner: false });
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+// NProgress.configure({ showSpinner: false });
+// Router.events.on("routeChangeStart", () => NProgress.start());
+// Router.events.on("routeChangeComplete", () => NProgress.done());
+// Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   const GTM_ID = "GTM-WQLRZPZ";
